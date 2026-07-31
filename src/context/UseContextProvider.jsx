acff07719edbb9ade5747e19d8fe3eefa52e5486
pawaraditya4899 <pawaraditya0337@gmail.com>
2026-07-31 23:51:13 +0530

    "due to maintain consistency"

--- src/context/UseContextProvider.jsx
+++ src/context/UseContextProvider.jsx
@@ -1,2 +1,12 @@
-// import react from "react";
+import React from "react";
+import UseContext from "./UseContext";
 
+const TodoContext = (props) => {
+    return (
+        <UseContext.Provider value={props.children}>
+            {props.children}
+        </UseContext.Provider>
+    );
+};
+
+export default TodoContext;
